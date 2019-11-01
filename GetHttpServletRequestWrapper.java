bebfa162a205f43fc2b669abfeda48d7f8f9d710
wangzhilong <wangzhilong@huatonghuhui.com>
2019-11-01 14:35:52 +0800

    zaicitijiaojuejue

--- GetHttpServletRequestWrapper.java
+++ GetHttpServletRequestWrapper.java
@@ -22,7 +22,9 @@ public class GetHttpServletRequestWrapper extends HttpServletRequestWrapper {
 	 * @param charset
 	 */
 	public GetHttpServletRequestWrapper(HttpServletRequest request, String charset) {
+		System.out.println("UTF-8");
 		super(request);
+		System.out.println("UTF-8");
 		this.charset = charset;
 	}
 
@@ -30,15 +32,20 @@ public class GetHttpServletRequestWrapper extends HttpServletRequestWrapper {
 	 * 调用被包装的请求对象的getParameter方法获得参数，然后再进行编码转换
 	 */
 	public String getParameter(String name) {
+		System.out.println("UTF-8");
 		String value = super.getParameter(name);
+		System.out.println("UTF-8");
 		value = value == null ? null : convert(value);
+		System.out.println("UTF-8");
 		return value;
 	}
 
 	public String convert(String target) {
 		try {
+			System.out.println("UTF-8");
 			return new String(target.trim().getBytes("ISO-8859-1"), charset);
 		} catch (UnsupportedEncodingException e) {
+			System.out.println("UTF-8");
 			return target;
 		}
 	}
